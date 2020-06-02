5d6b28fca19945638817d776478f61197eebc401
Oluwapelumi <pelumifafowora@gmail.com>
2020-06-02 00:44:26 -0400

    created the blueprint for the blockchain class as well as learned about the property decorator

--- Blockchain.py
+++ Blockchain.py
@@ -2,4 +2,26 @@
 
 #chain of blocks. Each block holds transactions
 class Blockchain:
-    pass
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        #Creates a new block and adds it to the chain
+        pass
+    
+    def add_new_transaction(self):
+        #Adds a new transaction to the list of current_transactions
+        pass
+    
+    @staticmethod #doesn't need an instance of this class to be executed. All objects share this
+    def hash(block):
+        #Hashes a block
+        pass
+
+    #makes the last block an attribute.
+    #retrieves the last value always since we can't simply set it to a static number since the last value may change.
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        pass
