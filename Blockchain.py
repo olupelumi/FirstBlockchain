b35732b845ace21de36010ec94622acbf474dac4
Oluwapelumi <pelumifafowora@gmail.com>
2020-06-04 00:13:03 -0400

    implemented the new block class

--- Blockchain.py
+++ Blockchain.py
@@ -1,5 +1,7 @@
 #creating a simple blockchain as a way to really understand how the blockchain works
-
+import hashlib
+import json
+from time import time
 #chain of blocks. Each block holds transactions
 #The Blockchain class is also responsible for adding new blocks to the chain.
 class Blockchain:
@@ -7,9 +9,31 @@ class Blockchain:
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
+    def new_block(self, proof, prev_hash = None):
         #Creates a new block and adds it to the chain
-        pass
+        """
+        Requires:
+        proof:<int> proof of work of the block
+        prev_hash: <str> Hash of the previous block
+
+        Effects:
+        Creates a new block and adds it to the chain
+        Returns the <dict> new block
+        """
+        block = {
+            'index': len(self.chain) + 1, #this new block will incrase length by 1
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'prev_hash': prev_hash or self.hash(self.chain[-1]) #if prev_hash is None then assigned to the return of self.hash
+        }
+
+        #resets the current list of transactions. 
+        self.current_transactions = [] #an empty list that will be populated with transactions for another block
+        
+        #adding the block to the chain
+        self.chain.append(block)
+        return block
     
     def add_new_transaction(self, sender, recipient, amount):
         #Adds a new transaction to the list of current_transactions in a block
