a10dce57fbdbbbab655fd90ebd588391d72ea507
Oluwapelumi <pelumifafowora@gmail.com>
2020-06-03 23:51:51 -0400

    implemented the add new transaction method

--- Blockchain.py
+++ Blockchain.py
@@ -1,6 +1,7 @@
 #creating a simple blockchain as a way to really understand how the blockchain works
 
 #chain of blocks. Each block holds transactions
+#The Blockchain class is also responsible for adding new blocks to the chain.
 class Blockchain:
     def __init__(self):
         self.chain = []
@@ -10,9 +11,25 @@ class Blockchain:
         #Creates a new block and adds it to the chain
         pass
     
-    def add_new_transaction(self):
-        #Adds a new transaction to the list of current_transactions
-        pass
+    def add_new_transaction(self, sender, recipient, amount):
+        #Adds a new transaction to the list of current_transactions in a block
+        """
+        Requires:
+        sender is a string address of the Sender node
+        recipient is a string address of the recipient node
+        amount: <int> Amount that was transacted
+
+        Effects:
+        Adds the new transaction to a list of transactions
+        Returns the index of the block that holds this transaction.
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
     
     @staticmethod #doesn't need an instance of this class to be executed. All objects share this
     def hash(block):
